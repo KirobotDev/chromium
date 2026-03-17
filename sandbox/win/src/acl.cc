efbbee5c7f2f6e442794858c95dd27da1a642976
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-03-17 20:14:16 +0100

    Update acl.cc

--- sandbox/win/src/acl.cc
+++ sandbox/win/src/acl.cc
@@ -5,45 +5,54 @@
 #include "sandbox/win/src/acl.h"
 
 #include <windows.h>
+#include <optional>
 
 #include "base/notreached.h"
 
 namespace sandbox {
 
+namespace {
+
+constexpr DWORD kMediumLowRid = SECURITY_MANDATORY_MEDIUM_RID - 2048;
+constexpr DWORD kBelowLowRid = SECURITY_MANDATORY_LOW_RID - 2048;
+
+}  
+
 std::optional<DWORD> GetIntegrityLevelRid(IntegrityLevel integrity_level) {
   switch (integrity_level) {
     case INTEGRITY_LEVEL_SYSTEM:
-      return DWORD{SECURITY_MANDATORY_SYSTEM_RID};
+      return SECURITY_MANDATORY_SYSTEM_RID;
     case INTEGRITY_LEVEL_HIGH:
-      return DWORD{SECURITY_MANDATORY_HIGH_RID};
+      return SECURITY_MANDATORY_HIGH_RID;
     case INTEGRITY_LEVEL_MEDIUM:
-      return DWORD{SECURITY_MANDATORY_MEDIUM_RID};
+      return SECURITY_MANDATORY_MEDIUM_RID;
     case INTEGRITY_LEVEL_MEDIUM_LOW:
-      return DWORD{SECURITY_MANDATORY_MEDIUM_RID - 2048};
+      return kMediumLowRid;
     case INTEGRITY_LEVEL_LOW:
-      return DWORD{SECURITY_MANDATORY_LOW_RID};
+      return SECURITY_MANDATORY_LOW_RID;
     case INTEGRITY_LEVEL_BELOW_LOW:
-      return DWORD{SECURITY_MANDATORY_LOW_RID - 2048};
+      return kBelowLowRid;
     case INTEGRITY_LEVEL_UNTRUSTED:
-      return DWORD{SECURITY_MANDATORY_UNTRUSTED_RID};
+      return SECURITY_MANDATORY_UNTRUSTED_RID;
     case INTEGRITY_LEVEL_LAST:
       return std::nullopt;
   }
 
   NOTREACHED();
+  return std::nullopt;
 }
 
 DWORD SetObjectIntegrityLabel(HANDLE handle,
                               base::win::SecurityObjectType object_type,
                               DWORD mandatory_policy,
                               IntegrityLevel integrity_level) {
-  std::optional<DWORD> value = GetIntegrityLevelRid(integrity_level);
-  if (!value) {
+  const std::optional<DWORD> rid = GetIntegrityLevelRid(integrity_level);
+  if (!rid) {
     return ERROR_INVALID_SID;
   }
 
   base::win::SecurityDescriptor sd;
-  if (!sd.SetMandatoryLabel(*value, 0, mandatory_policy)) {
+  if (!sd.SetMandatoryLabel(*rid, 0, mandatory_policy)) {
     return ::GetLastError();
   }
 
